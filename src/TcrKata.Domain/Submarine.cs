9f1e7ba95b34dfc377980443471706878b9d1d67
HunteRoi <me@tinaeldevresse.eu>
2022-02-25 16:16:05 +0100

    TCR

--- src/TcrKata.Domain/Submarine.cs
+++ src/TcrKata.Domain/Submarine.cs
@@ -2,22 +2,28 @@ namespace TcrKata.Domain;
 
 public class Submarine : ISubmarine
 {
+    private record Command(string Name, int Value);
+    private record State(int Aim, int Position, int Depth);
+
     public void ExecuteCommand(string command)
     {
         var commandObject = this.CreateCommand(command.Split(' '));
-        
+
         switch (commandObject.Name)
         {
             case "down":
                 this.Aim += commandObject.Value;
                 break;
+
             case "up":
                 this.Aim -= commandObject.Value;
                 break;
+
             case "forward":
                 this.Position += commandObject.Value;
                 this.Depth += this.Aim * commandObject.Value;
                 break;
+
             default:
                 break;
         }
@@ -26,10 +32,6 @@ public class Submarine : ISubmarine
     public int Aim { get; private set; }
     public int Position { get; private set; }
     public int Depth { get; private set; }
-
-    private record Command(string Name, int Value);
-
-    private record State(int Aim, int Position, int Depth);
 
     private Command CreateCommand(string[] commandTokens) => new(commandTokens[0], int.Parse(commandTokens[1]));
 }
