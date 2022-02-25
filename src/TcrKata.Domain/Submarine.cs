9805a54daceceddc5bb53b9e699620ff024d4f88
Tr00d <guillaume.faas@live.fr>
2022-02-25 18:26:44 +0100

    TCR

--- src/TcrKata.Domain/Submarine.cs
+++ src/TcrKata.Domain/Submarine.cs
@@ -1,35 +1,42 @@
-using LanguageExt;
-
 namespace TcrKata.Domain;
 
 /// <summary>
-/// pioup pioup
+///     pioup pioup
 /// </summary>
-/// <seealso cref="TcrKata.Domain.ISubmarine"/>
-/// <seealso cref="System.IDisposable"/>
+/// <seealso cref="TcrKata.Domain.ISubmarine" />
+/// <seealso cref="System.IDisposable" />
 public class Submarine : ISubmarine, IDisposable
 {
     /// <summary>
-    /// The state
-    /// </summary>
-    private State _state;
-
-    /// <summary>
-    /// The command parser
+    ///     The command parser
     /// </summary>
     private readonly ICommandParser _commandParser;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Submarine"/> class.
+    ///     The state
+    /// </summary>
+    private State _state;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Submarine" /> class.
     /// </summary>
     public Submarine()
     {
-        this._state = new(0, 0, 0);
+        this._state = new State(0, 0, 0);
         this._commandParser = new CommandParser();
     }
 
     /// <summary>
-    /// Executes the command.
+    ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+    ///     unmanaged resources.
+    /// </summary>
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    ///     Executes the command.
     /// </summary>
     /// <param name="command">The command.</param>
     public void ExecuteCommand(string command)
@@ -41,29 +48,20 @@ public class Submarine : ISubmarine, IDisposable
     }
 
     /// <summary>
-    /// Gets the aim.
+    ///     Gets the aim.
     /// </summary>
     /// <value>The aim.</value>
     public int Aim => this._state.Aim;
 
     /// <summary>
-    /// Gets the position.
+    ///     Gets the position.
     /// </summary>
     /// <value>The position.</value>
     public int Position => this._state.Position;
 
     /// <summary>
-    /// Gets the depth.
+    ///     Gets the depth.
     /// </summary>
     /// <value>The depth.</value>
     public int Depth => this._state.Depth;
-
-    /// <summary>
-    /// Performs application-defined tasks associated with freeing, releasing, or resetting
-    /// unmanaged resources.
-    /// </summary>
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-    }
 }
