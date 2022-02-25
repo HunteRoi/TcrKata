b2f5634bb30ac2c36c977a20eb3cb6f106a5943a
HunteRoi <me@tinaeldevresse.eu>
2022-02-25 18:18:54 +0100

    TCR

--- src/TcrKata.Domain/ICommand.cs
+++ src/TcrKata.Domain/ICommand.cs
@@ -1,10 +1,26 @@
 ﻿namespace TcrKata.Domain;
 
+/// <summary>
+/// A command executable in an instance of <see cref="Submarine"/>
+/// </summary>
 public interface ICommand
 {
+    /// <summary>
+    /// Gets the name.
+    /// </summary>
+    /// <value>The name.</value>
     string Name { get; }
 
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <value>The value.</value>
     int Value { get; }
 
+    /// <summary>
+    /// Executes the specified current state.
+    /// </summary>
+    /// <param name="currentState">State of the current.</param>
+    /// <returns></returns>
     State Execute(State currentState);
 }
