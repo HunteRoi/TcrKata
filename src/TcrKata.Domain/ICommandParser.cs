fead8a37476145e6b43e8774d64749c9746b75db
HunteRoi <me@tinaeldevresse.eu>
2022-02-25 18:18:29 +0100

    TCR

--- src/TcrKata.Domain/ICommandParser.cs
+++ src/TcrKata.Domain/ICommandParser.cs
@@ -1,6 +1,14 @@
 ﻿namespace TcrKata.Domain;
 
+/// <summary>
+/// A command parser for an instance of <see cref="Submarine"/>
+/// </summary>
 public interface ICommandParser
 {
+    /// <summary>
+    /// Creates the command.
+    /// </summary>
+    /// <param name="command">The command.</param>
+    /// <returns></returns>
     ICommand CreateCommand(string command);
 }
