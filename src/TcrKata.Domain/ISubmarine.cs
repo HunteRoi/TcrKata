6e02974e8ace2e47b92967179043cdbcce49dff8
HunteRoi <me@tinaeldevresse.eu>
2022-02-25 18:19:08 +0100

    TCR

--- src/TcrKata.Domain/ISubmarine.cs
+++ src/TcrKata.Domain/ISubmarine.cs
@@ -1,11 +1,31 @@
 namespace TcrKata.Domain;
 
+/// <summary>
+/// A submarine
+/// </summary>
 public interface ISubmarine
 {
+    /// <summary>
+    /// Gets the aim.
+    /// </summary>
+    /// <value>The aim.</value>
     int Aim { get; }
 
+    /// <summary>
+    /// Gets the position.
+    /// </summary>
+    /// <value>The position.</value>
     int Position { get; }
 
+    /// <summary>
+    /// Gets the depth.
+    /// </summary>
+    /// <value>The depth.</value>
     int Depth { get; }
+
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <param name="command">The command.</param>
     void ExecuteCommand(string command);
 }
